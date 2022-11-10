7a2a9bcf740bad808bfcdc48cb4f497f2e76d7db
Jens Luebeck <jluebeck@eng.ucsd.edu>
2022-11-10 07:33:51 -0800

    version 0.4.12
    full path on make input

--- make_input.sh
+++ make_input.sh
@@ -3,8 +3,9 @@
 #arg 1: location to search of AA files
 #arg 2: output name
 
-find $1 -name "*_cycles.txt" | grep -v "annotated_cycles" | sort > scf.txt
-find $1 -name "*_graph.txt" | grep -v "features_to_graph" | grep -v "feature_to_graph" | sort > sgf.txt
+exppath=`realpath $1`
+find $exppath -name "*_cycles.txt" | grep -v "annotated_cycles" | sort > scf.txt
+find $exppath -name "*_graph.txt" | grep -v "features_to_graph" | grep -v "feature_to_graph" | sort > sgf.txt
 if [ "$(wc -l < scf.txt)" -ne "$(wc -l < sgf.txt)" ]; then
   echo "ERROR: Unequal numbers of cycles and graph files found!"
   exit
